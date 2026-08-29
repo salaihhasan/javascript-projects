c2aea213885d61dd0f4babb029d0041c073db291
salaihhasan <salaihhasan@gmail.com>
2026-08-29 12:23:34 +0530

    Add product search app

---
+++ Product Search App/prod.js
@@ -0,0 +1,38 @@
+let products = document.getElementById("products");
+let result;
+
+async function renderProduct() {
+    const prod = await fetch("https://dummyjson.com/products");
+    const data = await prod.json();
+    result = data.products;
+   showAll(result);
+    
+}
+
+function showAll(items){
+   products.innerHTML = "";
+   items.map((arr) =>{
+       let card =  document.createElement("div");
+       card.className = "card";
+      card.innerHTML = `
+         <img src = "${arr.thumbnail}">
+         <div class = "title"><h3>${arr.title}<h3><div>
+         <div class = "price">Price : ${arr.price}<div>
+      `
+      products.appendChild(card);
+
+    })
+}
+
+document.getElementById("input").oninput = function (){
+   const input = document.getElementById("input").value.toLowerCase();
+   const newResult = result.filter(item =>{
+      return item.title.toLowerCase().includes(input);
+   })
+
+   showAll(newResult);
+}
+
+
+
+renderProduct();
